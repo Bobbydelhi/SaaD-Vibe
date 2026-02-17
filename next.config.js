e2c7a7e6fd1daf57edfd7e046c8aa8322b375c09
BobbyDelhi <budeli2006@gmail.com>
2026-02-17 18:20:04 +0100

    Fix Syntax error in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
- @type {import('next').NextConfig} 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    
-  },
-}
+  // Aquí puedes agregar más configuraciones si las necesitas en el futuro
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
